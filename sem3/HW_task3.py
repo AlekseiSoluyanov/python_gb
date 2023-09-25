ddce40a004d24541f5ed972fe38afbe103a3a3d1
Aleksey <alekc.col@mail.ru>
2023-09-25 19:04:27 +0300

    Add solution for HW_3

---
+++ sem3/HW_task3.py
@@ -0,0 +1,19 @@
+# Создайте словарь со списком вещей для похода в качестве ключа и их массой
+# в качестве значения. Определите какие вещи влезут в рюкзак передав
+# его максимальную грузоподъёмность.
+# Достаточно вернуть один допустимый вариант.
+
+stuff = {'matches': 1, 'cup': 2, 'tent': 10, 'food': 5, 'spare shoes': 3}
+
+
+def backpack_capacity(capacity: int, stuff: dict) -> list[str]:
+    packaging_option = []
+    summary = []
+    for key, value in stuff.items():
+        if value <= capacity:
+            capacity -= value
+            packaging_option.append(key)
+    return packaging_option
+
+
+print(backpack_capacity(15, stuff))
